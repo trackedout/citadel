d8b3bbd9e4129c060161381891b74a4cadb71c1e
4Ply <gamer.4ply@gmail.com>
2024-04-21 03:02:42 +0200

    Send a 'player-seen' event every second that a player is connected

--- src/main/kotlin/org/trackedout/citadel/listeners/PlayedJoinedListener.kt
+++ src/main/kotlin/org/trackedout/citadel/listeners/PlayedJoinedListener.kt
@@ -1,5 +1,6 @@
 package org.trackedout.citadel.listeners
 
+import com.destroystokyo.paper.event.server.ServerTickStartEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -10,7 +11,7 @@ import org.trackedout.client.models.EventsPostRequest
 
 class PlayedJoinedListener(
     private val plugin: Citadel,
-    private val eventsApi: EventsApi
+    private val eventsApi: EventsApi,
 ) : Listener {
     @EventHandler(ignoreCancelled = true)
     fun onPlayerJoin(event: PlayerJoinEvent) {
@@ -28,6 +29,27 @@ class PlayedJoinedListener(
             )
 
             plugin.logger.info("${player.name} joined the server. Dunga Dunga has been notified")
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onPlayerSeen(event: ServerTickStartEvent) {
+        if (event.tickNumber % 20 == 0) { // Every second
+            plugin.server.onlinePlayers.forEach { player ->
+                plugin.async(player) {
+                    eventsApi.eventsPost(
+                        EventsPostRequest(
+                            player = player.name,
+                            server = plugin.serverName,
+                            name = "player-seen",
+                            x = player.x,
+                            y = player.y,
+                            z = player.z,
+                            count = 1,
+                        )
+                    )
+                }
+            }
         }
     }
 }
