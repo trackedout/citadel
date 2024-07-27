348b36c461cc2a94838004882df9a2197cf8b18a
4Ply <gamer.4ply@gmail.com>
2024-07-27 09:33:02 +0200

    Teleport players out of dungeon entrance on reconnect

--- src/main/kotlin/org/trackedout/citadel/listeners/PlayedJoinedListener.kt
+++ src/main/kotlin/org/trackedout/citadel/listeners/PlayedJoinedListener.kt
@@ -1,6 +1,8 @@
 package org.trackedout.citadel.listeners
 
 import com.destroystokyo.paper.event.server.ServerTickStartEvent
+import org.bukkit.Location
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -16,6 +18,12 @@ class PlayedJoinedListener(
     @EventHandler(ignoreCancelled = true)
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
+        if (insideDungeonEntrance(player)) {
+            plugin.logger.info("${player.name} is within dungeon entrance at ${player.location}, teleporting them out")
+            player.teleport(Location(player.world, -512.0, 114.0, 1980.0, 90f, 0f))
+        }
+        plugin.logger.info("${player.name} joined at location: ${player.location}")
+
         plugin.async(player) {
             eventsApi.eventsPost(
                 Event(
@@ -30,6 +38,11 @@ class PlayedJoinedListener(
 
             plugin.logger.info("${player.name} joined the server. Dunga Dunga has been notified")
         }
+    }
+
+    private fun insideDungeonEntrance(player: Player): Boolean {
+        return -553 <= player.x && player.x <= -542
+            && 1977 <= player.z && player.z <= 1983
     }
 
     @EventHandler(ignoreCancelled = true)
